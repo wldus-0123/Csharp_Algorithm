f214049ff5248a516a54973cfae6dcd4d1a552f0
wldus-0123 <96jy@naver.com>
2024-01-24 17:37:51 +0900

    docs: Dijkstra

---
+++ 11. ShortesPath/Dijkstra.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._ShortesPath
+{
+    internal class Dijkstra
+    {
+        const int INF = 99999; // 단절되어있어용
+
+        public static void ShortestPath(int[,] graph, int start, out bool[] visited, out int[] distance, out int[] parents)
+        {
+            int size = graph.GetLength(0); // graph. GetLength(0) = graph의 행 크기 (정점 갯수)
+            visited = new bool[size]; // 방문 여부 체크
+            distance = new int[size]; // 정점에서부터의 거리
+            parents = new int[size];  // 바로 윗 경로
+
+            for(int i = 0; i < size; i++)  // 초기 셋팅
+            {
+                visited[i] = false; // 방문한 적 없어용
+                distance[i] = INF;  // 거리 무한대 상태입니당 ( 얼마나 걸리는지 아직 몰라요 )
+                parents[i] = -1; // 바로 윗 경로도 아직 몰라요
+            }
+
+            distance[start] = 0; // 시작지점 ~ 시작지점 거리를 0으로 설정
+
+            for(int i = 0; i < size; i++)  //  0번 정점부터 마지막 정점까지 1번씩 싹 돌기 ?
+            {
+                
+                // 1. 방문하지 않은 정점 중 가장 거리가 가까운 정점 선택
+
+                int next = -1; // 다음에 방문할 정점: -1은 없다는 뜻
+                int minDistance = INF; // 현시점 최소거리 ( 아직 모르므로 무한대로 표시 )
+
+                for(int j = 0; j < size; j++) 
+                {
+                    if (distance[j] < minDistance && !visited[j]) // 거리가 더 짧으면서, 방문하지 않은 정점일 경우
+                    {
+                        next = j; 
+                        minDistance = distance[j];  
+                    }
+                }
+
+                if (next < 0)
+                    break;
+
+                
+
+                // 2. 직접 연결된 거리보다 거쳐서 더 짧아지는 경우 갱신
+
+                for(int j = 0; j<size; j++)
+                {
+                    //  distance[j] : 목적지까지 직접 연결된 거리
+                    //  distance[next] : 탐색중인 정점까지의 거리
+                    //  graph[next, j] : 탐색중인 정점부터 목적지까지의 거리
+                    if (distance[j] > distance[next] + graph[next, j]) 
+                    {
+                        distance[j] = distance[next] + graph[next, j];
+                        parents[j] = next;
+                    }
+                }
+                visited[next] = true; // 걍 방문만하고 끝? visited : true / distance: 0 / parent -1 
+            }
+        }
+
+    }
+}
---
+++ 11. ShortesPath/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _11._ShortesPath
+{
+    internal class Program
+    {
+        /********************************************************************
+        * 데이크스트라 알고리즘 (Dijkstra Algorithm)
+        * 
+        * 특정한 노드에서 출발하여 다른 노드로 가는 각각의 최단 경로를 구함
+        * 방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드를 선택 후, (가장 가까운 정점 찾기)
+        * 해당 노드를 거쳐 다른 노드로 가는 비용 계산 (최단거리로 갱신해주기)
+        ********************************************************************/
+
+        const int INF = 99999;
+        static void Main(string[] args)
+        {
+            int[,] graph = new int[18, 18]
+            {
+                {0, 6, 6, INF, INF, INF, INF, 7, INF,INF,INF,INF,INF,INF,INF,INF,INF,INF},
+                {6, 0, INF, INF, INF, 9, INF, INF, INF,INF,INF,INF,INF,INF,INF,INF,INF,INF},
+                {6, INF, 0, 7, INF, INF, 8, INF, INF,INF,INF,INF,INF,INF,INF,INF,INF,INF},
+                {INF, INF, 7, 0, INF, INF, 8, INF, INF,INF,INF,INF,INF,INF,INF,INF,INF,3},
+                {INF, INF, INF, INF, 0, 2, INF, 7, 8,INF,INF,INF,INF,INF,INF,INF,INF,INF},
+                {INF, 9, INF, INF, 2, 0, 1, INF, INF,2,INF,INF,INF,INF,INF,INF,INF,INF},
+                {INF, INF, 8, 8, INF, 1, 0, INF, INF,2,8,INF,INF,INF,INF,INF,INF,INF},
+                {7, INF, INF, INF, 7, INF, INF, 0, 4,INF,INF,5,INF,INF,5,INF,INF,INF},
+                {INF, INF, INF, INF, 8, INF, INF, 4, 0, 3,INF,INF,4,INF,INF,INF,INF,INF},
+                {INF, INF, INF, INF, INF, 2, 2, INF, 3,0 ,5,INF,1,INF,INF,INF,INF,INF}, //9
+                {INF, INF, INF, INF, INF, INF, 8, INF, INF,5,0,INF,INF,INF,INF,INF,INF,7},
+                {INF, INF, INF, INF, INF, INF, INF, 5, INF,INF,INF,0,INF,INF,3,INF,INF,INF},
+                {INF, INF, INF, INF, INF, INF, INF, INF, 4,1,INF,INF,0,INF,INF,4,7,INF},
+                {INF, INF, INF, INF, INF, INF, INF, INF, INF,INF,INF,INF,INF,0,INF,INF,1,INF},
+                {INF, INF, INF, INF, INF, INF, INF, 5, INF,INF,INF,3,INF,INF,0,2,INF,INF},
+                {INF, INF, INF, INF, INF, INF, INF, INF, INF,INF,INF,INF,4,INF,2,0,3,6},
+                {INF, INF, INF, INF, INF, INF, INF, INF, INF,INF,INF,INF,7,1,INF,3,0,INF},
+                {INF, INF, INF, 3, INF, INF, INF, INF, INF,INF,7,INF,INF,INF,INF,6,INF,0},
+            };
+
+            Dijkstra.ShortestPath(graph, 0, out bool[] visited, out int[] distance, out int[] parents);
+
+        }
+    }
+}
